2cdbf6a8f478f9c513e84798f57aa2d98de1081c
Luis Valencia <lvalenciafarfan@gmail.com>
2026-04-28 18:40:11 -0500

    First commit - SistemSportR API

---
+++ UsuarioCanchaReserva/src/main/java/com/Sistem/UsuarioCanchaReserva/UsuarioCanchaReservaApplication.java
@@ -0,0 +1,13 @@
+package com.Sistem.UsuarioCanchaReserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioCanchaReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioCanchaReservaApplication.class, args);
+	}
+
+}
