78d631f881ca749a5a92b539afa892798e0470cf
Luis Valencia <lvalenciafarfan@gmail.com>
2026-04-30 13:58:33 -0500

    feat: add cancha controller

--- UsuarioCanchaReserva/src/main/java/com/Sistem/UsuarioCanchaReserva/controller/CanchaController.java
+++ UsuarioCanchaReserva/src/main/java/com/Sistem/UsuarioCanchaReserva/controller/CanchaController.java
@@ -1,4 +1,48 @@
 package com.Sistem.UsuarioCanchaReserva.controller;
 
+import com.Sistem.UsuarioCanchaReserva.entities.Cancha;
+import com.Sistem.UsuarioCanchaReserva.service.CanchaService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/canchas")
 public class CanchaController {
+
+    private final CanchaService canchaService;
+
+    public CanchaController(CanchaService canchaService){
+        this.canchaService = canchaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Cancha> crear(@RequestBody Cancha cancha){
+        return ResponseEntity.ok(canchaService.crearCancha(cancha));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cancha>> listar(){
+        return ResponseEntity.ok(canchaService.listarCanchas());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cancha> obtener(@PathVariable Long id){
+        return ResponseEntity.ok(canchaService.listarPorId(id));
+    }
+
+    @PutMapping("/{id}/precio")
+    public ResponseEntity<Cancha> actualizarPrecio(
+            @PathVariable Long id,
+            @RequestParam Long nuevoPrecio
+    ){
+        return ResponseEntity.ok(canchaService.actualizarPrecioH(id, nuevoPrecio));
+    }
+
+    @PutMapping("/{id}/disponibilidad")
+    public ResponseEntity<Cancha> cambiarDisponibilidad(@PathVariable Long id){
+        return ResponseEntity.ok(canchaService.cambiarDisponibilidad(id));
+    }
 }
